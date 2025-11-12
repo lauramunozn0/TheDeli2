f1e891e3d296dd8a37a25ac06a6cf2bd2a60b484
lauramunozn0 <lauramunozlaw@gmail.com>
2025-11-12 01:20:47 -0500

    started the sandwich subclass

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
 
 public class Sandwich extends Product {
+
+    private String size;
+    private String breadType;
+    private boolean toasted;
+
+    public Sandwich(String name, double basePrice, String size, String breadType, boolean toasted) {
+        super(name, basePrice);
+        this.size = size;
+        this.breadType = breadType;
+        this.toasted = toasted;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getBreadType() {
+        return breadType;
+    }
+
+    public boolean isToasted() {
+        return toasted;
+    }
+
+    public void setToasted(boolean toasted) {
+        this.toasted = toasted;
+
+    }
 }
