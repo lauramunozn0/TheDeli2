71eabf0f58b4a7b2c8ae8604603ec79e176b39d6
lauramunozn0 <lauramunozlaw@gmail.com>
2025-11-12 02:34:19 -0500

    override and price

--- src/main/java/com/pluralsight/Chip.java
+++ src/main/java/com/pluralsight/Chip.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
-public class Chip extends Product{
+public class Chip extends Product {
+
+    public Chip() {
+        super("Chips", 1.50);
+    }
+
+    @Override
+    public double getPrice() {
+        return 1.50;
+    }
+
+    @Override
+    public String toString() {
+        return "Chips - $" + String.format("%.2f", getPrice());
+    }
 }
