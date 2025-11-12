b619f0212f37cf9d1048836c647ce20083537935
lauramunozn0 <lauramunozlaw@gmail.com>
2025-11-11 23:59:58 -0500

    working on the homescreen options

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,30 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
 
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("~~~ Welcome to DELI-cious ~~~");
+            System.out.print("Please Enter your choice: ");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+
+
+            choice = Integer.parseInt(scanner.nextLine());
+
+            switch (choice) {
+                case 1 -> {
+                    Order order = new Order();
+                    order.startOrder();   //Ill be connecting with order
+                }
+                case 0 -> System.out.println("Goodbye!");
+                default -> System.out.println("Invalid choice, please try again.");
+            }
+        } while (choice != 0);
+    }
 }
