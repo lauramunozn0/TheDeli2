36f9f273f70a1577b5d7284908b8f564f46bf2de
lauramunozn0 <lauramunozlaw@gmail.com>
2025-11-12 00:27:38 -0500

    Abstract and normal attributes, constructor getters

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
 public abstract class Product {
+    private String name;
+    private double basePrice;
+
+    public Product(String name, double basePrice) {
+        this.name = name;
+        this.basePrice = basePrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
 }
