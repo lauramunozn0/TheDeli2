e34ee00b4ce682c5c08afe47c700d59ab849b6fa
lauramunozn0 <lauramunozlaw@gmail.com>
2025-11-12 00:16:10 -0500

    ArrayList

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,5 +1,11 @@
 package com.pluralsight;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Order {
+    private String customerName;
+    private String phoneNumber;
+    private boolean isMember;
+    private ArrayList<Product> products = new ArrayList<>();
 
 }
