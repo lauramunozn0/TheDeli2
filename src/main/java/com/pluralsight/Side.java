2e288fde8c6b54732e03f34b66b8a4b80a9fe6c3
lauramunozn0 <lauramunozlaw@gmail.com>
2025-11-12 20:12:28 -0500

    side subclass is extra no price one option

--- src/main/java/com/pluralsight/Side.java
+++ src/main/java/com/pluralsight/Side.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
 public class Side extends Toppings{
+    private boolean isExtra;
+
+    public Side(String name, boolean isExtra) {
+        super(name, 0.00);
+        this.isExtra = isExtra;
+    }
+
+    public boolean isExtra() {
+        return isExtra;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + (isExtra ? " (extra)" : "") + " (included)";
+    }
+
 }
